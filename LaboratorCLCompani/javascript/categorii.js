da5143ad02dd4ccca00ba18fdccfdee369702fad
Cezara Lungu <cezaralungu1@gmail.com>
2022-03-03 20:58:57 +0200

    release v1

---
+++ LaboratorCLCompani/javascript/categorii.js
@@ -0,0 +1,9 @@
+
+
+function openCategorii(){
+	document.getElementById('categorii_b').style.visibility = 'visible';
+}
+
+function closeCategorii(){
+	document.getElementById('categorii_b').style.visibility = 'hidden';
+}
